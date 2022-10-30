125ab8ed006d8f16ff61e41642d6d280d51fbf80
Juanmagw <juanma4x21@gmail.com>
2022-10-30 21:17:57 +0100

    Parte 7 Promises y Observables

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app.component';
 import { ButtonComponent } from './components/button/button.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { NotesService } from './services/notes.service';
+import { SocialLoginModule, SocialAuthServiceConfig } from '@abacritt/angularx-social-login';
+import { GoogleLoginProvider } from '@abacritt/angularx-social-login';
 @NgModule({
   declarations: [
     AppComponent
@@ -13,9 +15,28 @@ import { NotesService } from './services/notes.service';
     BrowserModule,
     AppRoutingModule,
     ButtonComponent,
-    NavbarComponent
+    NavbarComponent,
+    SocialLoginModule
   ],
-  providers: [NotesService],
+  providers: [NotesService,
+    {
+      provide: 'SocialAuthServiceConfig',
+      useValue: {
+        autoLogin: false,
+        providers: [
+          {
+            id: GoogleLoginProvider.PROVIDER_ID,
+            provider: new GoogleLoginProvider(
+              '1074876854626-lomnp9i09r9dc511dh0uc2jneoplfnd4.apps.googleusercontent.com'
+            )
+          }
+        ],
+        onError: (err) => {
+          console.error(err);
+        }
+      } as SocialAuthServiceConfig,
+    }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
